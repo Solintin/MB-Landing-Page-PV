042dd34034685629f9dcb7a85421b74cb9104ccd
Solintin <63493783+Solintin@users.noreply.github.com>
2021-05-16 10:24:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Footer.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container-fluid bg-success text-white px-3 pt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-3">
+          <div class="title-footer text-uppercase text-left">
+            Contact Us
+          </div>
+          <div class="d-flex flex-column">
+            <div>
+              <span><i class="fas fa-envelope pe-2"></i></span> 
+              supports@muslimbuilders.org
+            </div>
+            <div>
+              <span><i class="fas fa-phone-alt pe-2"></i></span>  +234 123-456-789
+            </div>
+            <div>
+              <span><i class="fas fa-map-marker-alt pe-2"></i></span>  Nigeria
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="text-uppercase title-footer">
+            Quick Links
+          </div>
+           <ul>
+          <li data-id="home"><router-link to="/home">Home</router-link></li>
+          <li data-id="about"><router-link to="/about">About</router-link></li>
+          <li data-id="team"><router-link to="/team">Team</router-link></li>
+          <li data-id="projects"><router-link to="/projects">Projects</router-link></li>
+        </ul>
+        </div>
+        <div class="col-md-4">
+          <div class="text-uppercase title-footer">
+            Social Handles
+          </div>
+          <div class="d-flex flex-column">
+            <div>
+              <span><i class="fab fa-facebook pe-2"></i></span>  MuslimBuilders
+            </div>
+            <div>
+              <span><i class="fab fa-twitter pe-2"></i></span>  MuslimBuilders
+            </div>
+            <div>
+              <span><i class="fab fa-linkedin pe-2"></i></span> MuslimBuilders
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center border-top py-4">
+        &copy; 2021 MuslimBuilders. All Rights Reserved.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  padding: 0;
+  li {
+    margin: 10px 0;
+  }
+}
+.d-flex {
+  div {
+    margin: 10px 0;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="top container-fluid  text-dark">
+    <div class="header-wrapper">
+      <div class="logo">
+        <a href="#">
+          <div class="fw-bold text-uppercase">
+            <div class=" text-white d-flex ">
+              <div class="m-text">M</div>
+
+              <div class="d-flex  part-logo flex-column">
+                <div class="space">uslim</div>
+                <div>Builders</div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="links">
+        <ul>
+          <li data-id="home"><router-link to="/home">Home</router-link></li>
+          <li data-id="about"><router-link to="/about">About</router-link></li>
+          <li data-id="team"><router-link to="/team">Team</router-link></li>
+          <li data-id="projects"><router-link to="/projects">Projects</router-link></li>
+        </ul>
+      </div>
+      <div class="menu-bar">
+        <div class="menu-wrapper">
+          <i class="text-white  fas fa-bars"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pagechecker: "",
+    };
+  },
+ 
+  mounted() {
+    const setThis = this;
+    const menuBar = document.querySelector(".menu-bar ");
+    const menuIcon = document.querySelector(".menu-bar i");
+    const menuLink = document.querySelector(".links");
+    menuBar.addEventListener("click", () => {
+      menuLink.classList.toggle("open");
+      menuIcon.classList.toggle("open");
+    });
+
+    const navHeader = document.querySelector(".top");
+ 
+    window.onscroll = () => {
+    document.documentElement.scrollTop > 100 ?
+        navHeader.classList.add("header-bg") :
+        navHeader.classList.remove("header-bg");
+      
+    };
+
+
+
+
+    setTimeout(() => {
+      const paths = document.querySelectorAll(".links li");
+
+     paths.forEach((path) => {
+        if (setThis.$route.path.trim() === `/${path.dataset.id}`) {
+          path.classList.add("active");
+        } else {
+          path.classList.remove("active");
+        }
+      });
+    }, 500);
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+body {
+  font-family: "Ubuntu", sans-serif;
+}
+.top {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 100;
+  &.header-bg{
+    background: rgba($color: #198754, $alpha: 0.7);
+    transition: all .6s ease-in-out;
+  }
+}
+.header-wrapper {
+  padding: 0 15px 0 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .logo {
+    z-index: 100;
+  }
+  li a {
+    font-size: 20px;
+    color: #fff;
+  }
+  ul {
+    margin: 0;
+    li {
+      display: inline-block;
+      padding: 5px 15px !important;
+      margin: 0 2px;
+      &:hover {
+        background: var(--Pri);
+        border-radius: 3px;
+      }
+    }
+    .active {
+      border: 1px solid var(--Pri);
+      border-radius: 3px;
+    }
+  }
+  .menu-bar {
+    display: none;
+    cursor: pointer;
+    z-index: 100;
+    .menu-wrapper {
+      border-radius: 50px;
+      border: 2px solid #fff;
+      padding: 10px;
+      height: 40px;
+      width: 40px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+  .m-text {
+    font-size: 50px;
+    margin: 0;
+  }
+  .space {
+    letter-spacing: 6.5px;
+  }
+  .part-logo {
+    margin-top: 15px;
+  }
+}
+
+// Mobile Navigation Menu
+@media screen and (max-width: 768px) {
+  .logo {
+    font-size: 12px;
+    .m-text {
+      font-size: 35px;
+      margin: 0;
+    }
+    .part-logo {
+      margin-top: 8px;
+    }
+    .space {
+      letter-spacing: 5px;
+    }
+  }
+  .menu-bar {
+    display: block !important;
+  }
+  .links {
+    height: 100vh;
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    background: rgba($color: #198754, $alpha: 0.9);
+    clip-path: circle(10px at 90% -12%);
+    -webkit-clip-path: circle(10px at 90% -12%);
+    transition: all 1s ease-out;
+
+    &.open {
+      clip-path: circle(1000px at 90% -12%);
+      -webkit-clip-path: circle(1000px at 90% -12%);
+    }
+
+    li {
+      margin: 70px 70px !important;
+      display: flex !important;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-end;
+      font-size: 40px;
+
+      &.active {
+        border: none;
+      }
+    }
+  }
+  .menu-bar i.open::before {
+    content: "\f00d";
+    transition: all 1s ease-in;
+  }
+}
+</style>
